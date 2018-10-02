72232bf4a1f77676630489cd70726c71da87c50f
Flokami <ferrereflorent@hotmail.fr>
2018-10-03 00:02:04 +0400

    Add a assert for the server response test

--- tests/test_gmap.py
+++ tests/test_gmap.py
@@ -1,6 +1,7 @@
 import pytest
-
+import json
 import sys
+
 sys.path.append("../")
 
 from gmap import *
@@ -11,8 +12,9 @@ def test_create_map():
 	assert map.name == "map"
 
 def test_map_server_response():
-	response = map.getLocation("Reunion")
+	response = map.getLocation("Réunion")
 	assert response["status"] == "OK"
+	assert response['results'][0]['address_components'][0]['long_name'] == "Réunion"
 
 
 
