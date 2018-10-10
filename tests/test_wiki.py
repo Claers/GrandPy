e48e0f62b748897a45f05a66c8455635fbae6c86
Flokami <ferrereflorent@hotmail.fr>
2018-10-10 15:09:46 +0400

    Test_wiki corrections

--- tests/test_wiki.py
+++ tests/test_wiki.py
@@ -14,7 +14,7 @@ wikiClient = Wiki("wikiclient")
 def test_create_wiki():
 	assert wikiClient.name == "wikiclient"
 
-def test_get_article(monkeypatch):
+def test_get_article_with_location(monkeypatch):
 	results = {
 		'batchcomplete': '', 'query': {'pages': {'7508': {'pageid': 7508, 'ns': 0, 'title': 'La Réunion', 'index': 0, 'terms': {'description': ["département et région d'outre-mer français situé dans l'Océan indien"]}, 'contentmodel': 'wikitext', 'pagelanguage': 'fr', 'pagelanguagehtmlcode': 'fr', 'pagelanguagedir': 'ltr', 'touched': '2018-10-03T11:08:55Z', 'lastrevid': 152681604, 'length': 117457, 'fullurl': 'https://fr.wikipedia.org/wiki/La_R%C3%A9union', 'editurl': 'https://fr.wikipedia.org/w/index.php?title=La_R%C3%A9union&action=edit', 'canonicalurl': 'https://fr.wikipedia.org/wiki/La_R%C3%A9union'}}}
 	}
@@ -24,11 +24,11 @@ def test_get_article(monkeypatch):
 
 	monkeypatch.setattr(requests, 'get', mockreturn)
 	
-	resp = wikiClient.getArticle({'lat': -21.115141, 'lng': 55.53638400000001},"1000")
+	resp = wikiClient.getArticleWithLocation({'lat': -21.115141, 'lng': 55.53638400000001},"1000")
 	resp = json.loads(resp)
 	assert resp['query']['pages']['7508']['pageid'] == 7508
 
-def test_get_article_Desc():
-	response = wikiClient.getArticleDesc("France")
-	assert response == "La France (), en forme longue depuis 1875 la République française (), est un État transcontinental souverain, dont le territoire métropolitain est situé en Europe de l'Ouest."
+def test_get_article_with_name():
+	response = wikiClient.getArticleWithName("France")
+	assert response[1][0] == "France"
 
